15ffb15f3a52246125221c0b35be65b1b3188640
Ivan Zaichenko <i.v.zaichenko70@gmail.com>
2019-04-20 11:22:23 +0300

    Add files EditController, edit.ftl, useredit.ftl, userListFtl, applicationproperties

---
+++ src/main/java/com.company/CloudStorage/controller/EditController.java
@@ -0,0 +1,70 @@
+package com.company.CloudStorage.controller;
+
+import com.example.sweater.domain.Message;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@Controller
+public class EditController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    private String tempNameFile;
+
+
+    @GetMapping("/edit")
+    public String edit(@RequestParam String editFile, Model model) {
+        tempNameFile = editFile;
+        Iterable<Message> messages = messageRepo.findAll() ;
+        for (Message element:messages
+        ) {
+            if(element.getContainsFile().equals(editFile))
+                model.addAttribute("messageEdit", element);
+        }
+        return "edit";
+    }
+
+
+    @PostMapping("/edit")
+    public String edit2(@RequestParam String editFile, Map<String, Object> model) throws FileNotFoundException, UnsupportedEncodingException {
+
+        Iterable<Message> messages = messageRepo.findAll() ;
+        messageRepo.deleteAll();
+
+        for (Message element:messages
+        ) {
+            if(element.getContainsFile().equals(tempNameFile)) {
+                element.setContainsFile(editFile);
+                PrintWriter writer = new PrintWriter(uploadPath + "/" + "update"+element.getNameFile(), "UTF-8");
+                writer.println(element.getContainsFile());
+                writer.close();
+                // File file = new File( uploadPath + "/" + "update"+element.getNameFile());
+
+            }
+            messageRepo.save(element);
+
+        }
+
+        return "redirect:/main";
+    }
+
+}
